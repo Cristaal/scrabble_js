946acffbb0a24373a1171a60b39ebc64d81448f3
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-02-12 20:51:58 -0800

    add specs for multiple letter scores and ignoring apostrophes

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,24 @@
-var points = {1:["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]};
+var points1 = {1:["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]};
+var points2 = {2:["D", "G"]};
+var points3 = {3:["B", "C", "M", "P"]};
+var points4 = {4:["F", "H", "V", "W", "Y"]};
+var points5 = {5:["K"]};
+var points8 = {8:["J", "X"]};
+var points10 = {10:["Q", "Z"]};
 
 var scrabbleScore = function(word) {
-  var newWord  = word.toUpperCase().split('');
+  var newWord  = word.replace(/\'\b/g, " ").toUpperCase().split('');
   var score = 0;
 
 for (var i = 0; i < newWord.length; i++) {
   var letter = newWord[i];
-  for (var n= 0; n < 10; n++) {
-    if(letter === points[1][n]) {
+  for (var n = 0; n < 10; n++) {
+    if(letter === points1[1][n]) {
       score += 1;
+    } else if (letter === points2[2][n]) {
+      score += 2;
+    } else if (letter === points3[3][n]) {
+      score += 3;
     }
   }
 }
--- spec/specs.js
+++ spec/specs.js
@@ -5,4 +5,13 @@ describe('scrabbleScore', function() {
   it("can return a score for a word made of any combination of 1-point letters", function() {
     expect(scrabbleScore("learn")).to.equal(5);
   });
+  it("can return a score for words with two-point letter values", function() {
+    expect(scrabbleScore("dog")).to.equal(5);
+  });
+  it("can return a score for words with three-point letter values", function() {
+    expect(scrabbleScore("bump")).to.equal(10);
+  });
+  it("can ignore apostrophes", function() {
+    expect(scrabbleScore("bill's")).to.equal(7);
+  });
 });
